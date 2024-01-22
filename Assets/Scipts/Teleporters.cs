5da754d62cb036c5c5d2ee5c773d0aa99765c506
Jay <91475458+JayK1tsune@users.noreply.github.com>
2024-01-22 15:42:21 +0000

    22/01/2024
    Updated TP:
    Now is Random where they go.
    Can be used for more controlled Tp'ing when we have ironed out how map will spawn.
    
    Test room to be completed on Tuesday with Designers to test if we can spawn a Prefab with a list of viable TP Locations.

--- Assets/Scipts/Teleporters.cs
+++ Assets/Scipts/Teleporters.cs
@@ -1,14 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
 {
-    [SerializeField] Transform _destination;
+    [SerializeField] List<Transform> _teleporters;
+    private Transform _currentTeleporter;
 
+    private void Start()
+    {
+        StartCoroutine(ChangeTeleporterPeriodically(5f));
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && other.TryGetComponent(out PlayerController player))
         {
+            Transform _destination = GetRandomTeleporter();
             player.Teleport(_destination.position);
             Debug.Log("Teleporting");
         }
@@ -16,9 +24,51 @@ public class TeleportScript : MonoBehaviour
     }
     void OnDrawGizmos()
     {
+        
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(_destination.position, 4f);
-        var direction = _destination.TransformDirection(Vector3.forward);
-        Gizmos.DrawRay(_destination.position, direction);
+        foreach (Transform teleporter in _teleporters)
+        {
+            Gizmos.DrawWireSphere(teleporter.position, 4f);
+            var direction = teleporter.TransformDirection(Vector3.forward);
+            Gizmos.DrawRay(teleporter.position, direction);
+        }
     }
+
+    Transform GetRandomTeleporter()
+    {
+        if (_teleporters.Count == 0)
+        {
+            return null;
+        }
+
+        int randomIndex = Random.Range(0, _teleporters.Count);
+        return _teleporters[randomIndex];
+    }
+
+    void Update()
+    {
+        Transform _destination = GetRandomTeleporter();
+        Debug.Log(_destination);
+    }
+
+    private IEnumerator ChangeTeleporterPeriodically(float interval)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+
+            // Get a new random teleporter
+            _currentTeleporter = GetRandomTeleporter();
+
+            if (_currentTeleporter != null)
+            {
+                Debug.Log("Current teleporter changed");
+            }
+            else
+            {
+                Debug.LogWarning("No teleporters available");
+            }
+        }
+    }
+   
 }
