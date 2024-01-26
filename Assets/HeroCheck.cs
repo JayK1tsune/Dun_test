a40920f436a3f44731801dfd52991a0ebc0b7174
Jay <91475458+JayK1tsune@users.noreply.github.com>
2024-01-26 14:19:20 +0000

    ..

---
+++ Assets/HeroCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCheck : MonoBehaviour
+{
+    Animator anim;
+    GameObject hero;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        hero = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(hero == other.gameObject)
+        {
+            anim.SetBool("EnemyClose", true);
+        }
+    }
+}
